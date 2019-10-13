9195d38863abecd3e627d41b70f23db1260c9f82
Markus Petters <mdpetter@ncsu.edu>
2019-10-12 20:59:59 -0400

    Fix function name

--- src/gtk_callbacks.jl
+++ src/gtk_callbacks.jl
@@ -2,10 +2,10 @@
 # Connect dropdown menus with callback functions...
 gSelect1= gui["xpxp1G1"]
 signal_connect(gSelect1, "changed") do widget, others...
-	update_turbulence()
+	update_graph()
 end
 
 gSelect2= gui["xpxp1G2"]
 signal_connect(gSelect2, "changed") do widget, others...
-	update_turbulence()
+	update_graph()
 end
